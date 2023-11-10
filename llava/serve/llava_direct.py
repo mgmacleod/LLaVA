1ed32e3071e3a9f84912b108da6fbe84e59bacde
Matthew MacLeod <mgmacleod8@gmail.com>
2023-11-10 15:41:12 -0500

    working if crappy implementation

--- llava/serve/llava_direct.py
+++ llava/serve/llava_direct.py
@@ -1,4 +1,3 @@
-import argparse
 from io import BytesIO
 
 import requests
@@ -39,7 +38,7 @@ class LlavaDirect:
         # Model
         disable_torch_init()
 
-        self.args = self.parse_args()
+        # self.args = self.parse_args()
         self.model = None
         self.tokenizer = None
         self.image_processor = None
@@ -49,6 +48,10 @@ class LlavaDirect:
         self.image = None
         self.image_tensor = None
         self.conv_mode = None
+        self.debug = debug
+        self.model_name = None
+        self.temperature = temperature
+        self.max_new_tokens = max_new_tokens
 
         self.load_model(model_path, model_base, load_8bit, load_4bit, device)
 
@@ -64,16 +67,10 @@ class LlavaDirect:
         self.conv = conv_templates[self.conv_mode].copy()
         if "mpt" in self.model_name.lower():
             self.roles = ("user", "assistant")
-        else:
             self.roles = self.conv.roles
 
-    def parse_args(self):
-        parser = argparse.ArgumentParser()
-        # add arguments here
-        return parser.parse_args()
-
     def load_model(self, model_path, model_base, load_8bit, load_4bit, device):
-        model_name = get_model_name_from_path(model_path)
+        self.model_name = get_model_name_from_path(model_path)
         (
             self.tokenizer,
             self.model,
@@ -82,20 +79,90 @@ class LlavaDirect:
         ) = load_pretrained_model(
             model_path,
             model_base,
-            model_name,
+            self.model_name,
             load_8bit,
             load_4bit,
             device=device,
         )
 
     def run(self):
-        # main execution code here
+        image = self.load_image("https://llava-vl.github.io/static/images/view.jpg")
+        self.process_image(image, "what do you see here?")
+
         return
 
-    def load_image(image_file):
+    def load_image(self, image_file):
         if image_file.startswith("http://") or image_file.startswith("https://"):
             response = requests.get(image_file)
             image = Image.open(BytesIO(response.content)).convert("RGB")
         else:
             image = Image.open(image_file).convert("RGB")
         return image
+
+    def process_image(self, image, inp):
+        # Similar operation in model_worker.py
+        image_tensor = process_images([image], self.image_processor, self.model.config)
+        if type(image_tensor) is list:
+            image_tensor = [
+                image.to(self.model.device, dtype=torch.float16)
+                for image in image_tensor
+            ]
+        else:
+            image_tensor = image_tensor.to(self.model.device, dtype=torch.float16)
+        # return image_tensor
+
+        if image is not None:
+            # first message
+            if self.model.config.mm_use_im_start_end:
+                inp = (
+                    DEFAULT_IM_START_TOKEN
+                    + DEFAULT_IMAGE_TOKEN
+                    + DEFAULT_IM_END_TOKEN
+                    + "\n"
+                    + inp
+                )
+            else:
+                inp = DEFAULT_IMAGE_TOKEN + "\n" + inp
+            self.conv.append_message(self.conv.roles[0], inp)
+            image = None
+
+        self.conv.append_message(self.conv.roles[1], None)
+        prompt = self.conv.get_prompt()
+
+        input_ids = (
+            tokenizer_image_token(
+                prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
+            )
+            .unsqueeze(0)
+            .to(self.model.device)
+        )
+        stop_str = (
+            self.conv.sep
+            if self.conv.sep_style != SeparatorStyle.TWO
+            else self.conv.sep2
+        )
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(
+            keywords, self.tokenizer, input_ids
+        )
+        streamer = TextStreamer(
+            self.tokenizer, skip_prompt=True, skip_special_tokens=True
+        )
+
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                input_ids,
+                images=image_tensor,
+                do_sample=True if self.temperature > 0 else False,
+                temperature=self.temperature,
+                max_new_tokens=self.max_new_tokens,
+                streamer=streamer,
+                use_cache=True,
+                stopping_criteria=[stopping_criteria],
+            )
+
+        outputs = self.tokenizer.decode(output_ids[0, input_ids.shape[1] :]).strip()
+        self.conv.messages[-1][-1] = outputs
+
+        if self.debug:
+            print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
